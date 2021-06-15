4287ae834d40d08b5d92582326b9e1a3510e2d8e
Jace <bellcastle88@gmail.com>
2021-06-15 23:57:15 +0900

    Commit

--- src/index.js
+++ src/index.js
@@ -4,23 +4,41 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css'
 
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/519rMWsoNiL._SX325_BO1,204,203,200_.jpg',
+  title: 'Hood Feminism',
+  author: 'Mikki Kendall',
+}
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/61XOZFGGTZL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  title: 'The Color of Law',
+  author: 'Richard Rothstein',
+}
+
 function BookList() {
   return ( 
     <section className='booklist'>
-      <Book />
+      <Book 
+        img={firstBook.img} 
+        title={firstBook.title}
+        author={firstBook.author} 
+      />
+      <Book 
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const author = 'Jace.K';
-const Book = () => {
-  const title = 'IRRESISTIBLE HUSBAND';
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="https://cdn.shopify.com/s/files/1/1269/2171/products/IRRHUSB1_large.jpg?v=1477272293" alt="" />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      <p>{1+5}</p>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }
