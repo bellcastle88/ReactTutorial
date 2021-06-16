7aeb71f01eafa406eb16d515a3047d6e37aa8a78
Jace <bellcastle88@gmail.com>
2021-06-16 18:16:05 +0900

    Commit

--- src/index.js
+++ src/index.js
@@ -4,34 +4,28 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css'
 
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/519rMWsoNiL._SX325_BO1,204,203,200_.jpg',
-  title: 'Hood Feminism',
-  author: 'Mikki Kendall',
-}
-
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/61XOZFGGTZL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
-  title: 'The Color of Law',
-  author: 'Richard Rothstein',
-}
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/519rMWsoNiL._SX325_BO1,204,203,200_.jpg',
+    title: 'Hood Feminism',
+    author: 'Mikki Kendall',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61XOZFGGTZL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'The Color of Law',
+    author: 'Richard Rothstein',
+  },
+];
 
 function BookList() {
   return ( 
     <section className='booklist'>
-      <Book 
-        img={firstBook.img} 
-        title={firstBook.title}
-        author={firstBook.author} 
-      />
-      <Book 
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {names}
     </section>
   );
 }
+
+const names = ['jhon', 'kane', 'son'];
 
 const Book = (props) => {
   return (
